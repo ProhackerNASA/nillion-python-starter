45620946768832bc76280652ef81a1690a5381ce
ProhackerNASA <sharma.akhil.school@gmail.com>
2024-07-05 04:25:39 +0000

    my new nada program

---
+++ quickstart/client_code/run_my_first_program.py
@@ -0,0 +1,120 @@
+import asyncio
+import py_nillion_client as nillion
+import os
+
+from py_nillion_client import NodeKey, UserKey
+from dotenv import load_dotenv
+from nillion_python_helpers import get_quote_and_pay, create_nillion_client, create_payments_config
+
+from cosmpy.aerial.client import LedgerClient
+from cosmpy.aerial.wallet import LocalWallet
+from cosmpy.crypto.keypairs import PrivateKey
+
+home = os.getenv("HOME")
+load_dotenv(f"{home}/.config/nillion/nillion-devnet.env")
+
+async def main():
+    # 1. Initial setup
+    cluster_id = os.getenv("NILLION_CLUSTER_ID")
+    grpc_endpoint = os.getenv("NILLION_NILCHAIN_GRPC")
+    chain_id = os.getenv("NILLION_NILCHAIN_CHAIN_ID")
+
+    seed = "my_seed"
+    userkey = UserKey.from_seed(seed)
+    nodekey = NodeKey.from_seed(seed)
+
+    client = create_nillion_client(userkey, nodekey)
+
+    party_id = client.party_id
+    user_id = client.user_id
+
+    program_name = "secret_multiplication_complete"
+    program_mir_path = f"/content/nillion-python-starter/quickstart/nada_quickstart_programs/target/main.nada.bin"
+
+    payments_config = create_payments_config(chain_id, grpc_endpoint)
+    payments_client = LedgerClient(payments_config)
+    payments_wallet = LocalWallet(
+        PrivateKey(bytes.fromhex(os.getenv("NILLION_NILCHAIN_PRIVATE_KEY_0"))),
+        prefix="nillion",
+    )
+
+    receipt_store_program = await get_quote_and_pay(
+        client,
+        nillion.Operation.store_program(program_mir_path),
+        payments_wallet,
+        payments_client,
+        cluster_id,
+    )
+
+    action_id = await client.store_program(
+        cluster_id, program_name, program_mir_path, receipt_store_program
+    )
+
+    program_id = f"{user_id}/{program_name}"
+    print("Stored program. action_id:", action_id)
+    print("Stored program_id:", program_id)
+
+    new_secret = nillion.NadaValues(
+        {
+            "my_int1": nillion.SecretInteger(20),
+        }
+    )
+
+    party1_name = "Party1"
+
+    permissions = nillion.Permissions.default_for_user(client.user_id)
+    permissions.add_compute_permissions({client.user_id: {program_id}})
+
+    receipt_store = await get_quote_and_pay(
+        client,
+        nillion.Operation.store_values(new_secret, ttl_days=5),
+        payments_wallet,
+        payments_client,
+        cluster_id,
+    )
+
+    store_id = await client.store_values(
+        cluster_id, new_secret, permissions, receipt_store
+    )
+    print(f"Computing using program {program_id}")
+    print(f"Use secret store_id: {store_id}")
+
+    compute_bindings = nillion.ProgramBindings(program_id)
+    compute_bindings.add_input_party(party1_name, party_id)
+    compute_bindings.add_input_party("Party2", party_id)
+    compute_bindings.add_output_party(party1_name, party_id)
+    compute_bindings.add_output_party("Party3", party_id)
+
+    computation_time_secrets = nillion.NadaValues({"my_int2": nillion.SecretInteger(15)})
+
+    receipt_compute = await get_quote_and_pay(
+        client,
+        nillion.Operation.compute(program_id, computation_time_secrets),
+        payments_wallet,
+        payments_client,
+        cluster_id,
+    )
+
+    compute_id = await client.compute(
+        cluster_id,
+        compute_bindings,
+        [store_id],
+        computation_time_secrets,
+        receipt_compute,
+    )
+
+    print(f"The computation was sent to the network. compute_id: {compute_id}")
+    while True:
+        compute_event = await client.next_compute_event()
+        if isinstance(compute_event, nillion.ComputeFinishedEvent):
+            print(f"✅  Compute complete for compute_id {compute_event.uuid}")
+            print(f"🖥️  The result is {compute_event.result.value}")
+            return compute_event.result.value
+        elif isinstance(compute_event, nillion.ComputeFailedEvent):
+            print(f"❌  Compute failed for compute_id {compute_event.uuid}")
+            print(f"Error: {compute_event.error_message}")
+            break
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
